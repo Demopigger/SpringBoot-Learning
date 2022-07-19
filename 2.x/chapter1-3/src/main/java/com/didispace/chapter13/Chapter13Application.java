99a09a41c1d83da71217bc48b5a70e7bbf11f92b
demopigger <1599489431@qq.com>
2022-07-19 20:03:43 +0800

    增加日志实例

--- 2.x/chapter1-3/src/main/java/com/didispace/chapter13/Chapter13Application.java
+++ 2.x/chapter1-3/src/main/java/com/didispace/chapter13/Chapter13Application.java
@@ -1,6 +1,8 @@
 package com.didispace.chapter13;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,7 +20,7 @@ public class Chapter13Application {
     @Slf4j
     @RestController
     static class HelloController {
-
+        Logger log = LoggerFactory.getLogger(getClass());
         @Value("${db:}")
         private String db;
 
