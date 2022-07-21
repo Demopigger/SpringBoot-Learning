a8b4ebcbe0bff81563fc53abf9018514b56a516b
demopigger <1599489431@qq.com>
2022-07-21 23:25:58 +0800

    测试加解密
    测试加解密

--- 2.x/chapter1-5/src/test/java/com/didispace/chapter15/PropertiesTest.java
+++ 2.x/chapter1-5/src/test/java/com/didispace/chapter15/PropertiesTest.java
@@ -1,7 +1,10 @@
 package com.didispace.chapter15;
 
 import lombok.extern.slf4j.Slf4j;
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +12,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @Slf4j
 @SpringBootTest
 public class PropertiesTest {
+    Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private StringEncryptor stringEncryptor;
 
     @Value("${datasource.password:}")
     private String password;
@@ -17,5 +24,14 @@ public class PropertiesTest {
     public void test() {
         log.info("datasource.password : {}", password);
     }
+
+    @Test
+    public void jasyptTest() {
+        // 加密
+        System.out.println(stringEncryptor.encrypt("root"));    // JSrINYe4IBotHndGjX1hnmY3mtPNUJlXjP12cx1+pHqUz2FNXGPu3Frnajh3QCXg
+        // 解密
+        System.out.println(stringEncryptor.decrypt("1zWzvCYOGpvC8Ek80qjm3VPkTgmB3s+hlzfOK08LL22RgPhozrtr+hSqqc7pBwrv"));    // root
+    }
+
 
 }
